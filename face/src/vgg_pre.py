2776a8d027d5cc59c76e10efd8e081ee252ff2aa
azhe825 <zyu9@ncsu.edu>
2024-01-28 10:20:44 -0500

    epochs

--- face/src/vgg_pre.py
+++ face/src/vgg_pre.py
@@ -101,7 +101,7 @@ class VGG_Pre:
                                                           )
 
         history = self.model.fit(X, y, sample_weight=sample_weight, callbacks=[lr_reduce, checkpointer],
-                                 validation_data=(X_val, y_val, val_sample_weights), batch_size=10, epochs=100, verbose=1)
+                                 validation_data=(X_val, y_val, val_sample_weights), batch_size=10, epochs=10, verbose=1)
 
         # lr_reduce = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', patience=10, verbose=1, mode='auto',
         #                                                  min_lr=5e-5)
