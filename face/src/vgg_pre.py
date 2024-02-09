19989593984fa0473001ce2674235c733bd00503
azhe825 <zyu9@ncsu.edu>
2024-02-09 14:44:04 -0500

    sequence

--- face/src/vgg_pre.py
+++ face/src/vgg_pre.py
@@ -80,8 +80,8 @@ class VGG_Pre:
             # base_model_output = tf.keras.layers.Dropout(0.5)(base_model_output)
             base_model_output = tf.keras.layers.Dense(1, activation='sigmoid')(base_model_output)
 
-            # self.model = tf.keras.Model(inputs=base_model.input, outputs=base_model_output)
-            self.model = FullBatchModel(inputs=base_model.input, outputs=base_model_output)
+            self.model = tf.keras.Model(inputs=base_model.input, outputs=base_model_output)
+            # self.model = FullBatchModel(inputs=base_model.input, outputs=base_model_output)
             self.model.compile(loss=tf.keras.losses.BinaryCrossentropy(), metrics=['accuracy'], weighted_metrics=[tf.keras.metrics.BinaryCrossentropy()], optimizer='SGD')
         else:
             self.load_model(saved_model)
@@ -103,7 +103,7 @@ class VGG_Pre:
         earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=50, min_delta=1e-4)
 
         history = self.model.fit(X, y, sample_weight=sample_weight, callbacks=[lr_reduce, checkpointer, earlystop],
-                                 validation_data=(X_val, y_val, val_sample_weights), batch_size=len(y), epochs=200, verbose=1)
+                                 validation_data=(X_val, y_val, val_sample_weights), batch_size=100, epochs=1000, verbose=1)
 
         # lr_reduce = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', patience=10, verbose=1, mode='auto',
         #                                                  min_lr=5e-5)
@@ -114,6 +114,26 @@ class VGG_Pre:
         #                                                   )
         # history = self.model.fit(X, y, sample_weight=sample_weight, callbacks=[lr_reduce, checkpointer], batch_size=10, epochs=100,
         #                          verbose=1)
+        self.load_model('checkpoint/attractiveness.keras')
+        print(history.history)
+
+    def fit2(self, train_data, val_data):
+        # pre-trained weights of vgg-face model.
+        # you can find it here: https://drive.google.com/file/d/1CPSeum3HpopfomUEK1gybeuIVoeJT_Eo/view?usp=sharing
+        # related blog post: https://sefiks.com/2018/08/06/deep-face-recognition-with-keras/
+
+        lr_reduce = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', patience=10, verbose=1, mode='auto',
+                                                         min_lr=5e-5)
+
+        checkpointer = tf.keras.callbacks.ModelCheckpoint(filepath='checkpoint/attractiveness.keras'
+                                                          , monitor="val_loss", verbose=1
+                                                          , save_best_only=True, mode='auto'
+                                                          )
+        earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=50, min_delta=1e-4)
+
+        history = self.model.fit(train_data, callbacks=[lr_reduce, checkpointer, earlystop],
+                                 validation_data=val_data, epochs=1000, verbose=1)
+
         self.load_model('checkpoint/attractiveness.keras')
         print(history.history)
 
@@ -129,47 +149,47 @@ class VGG_Pre:
     def load_model(self, checkpoint_filepath):
         self.model = tf.keras.models.load_model(checkpoint_filepath)
 
-class FullBatchModel(tf.keras.Model):
-    def train_step(self, data):
-        # Unpack the data. Its structure depends on your model and
-        # on what you pass to `fit()`.
-        if len(data) == 3:
-            x, y, sample_weight = data
-        else:
-            sample_weight = None
-            x, y = data
-
-        small_batch = 10
-        gradients = None
-        for i in range(0, y.shape[0], small_batch):
-            xx = x[i:i+small_batch]
-            yy = y[i:i+small_batch]
-            if sample_weight is None:
-                ss = None
-            else:
-                ss = sample_weight[i:i+small_batch]
-            with tf.GradientTape() as tape:
-                y_pred = self(xx, training=True)  # Forward pass
-                # Compute the loss value
-                # (the loss function is configured in `compile()`)
-                loss = self.compute_loss(y=yy, y_pred=y_pred, sample_weight=ss)
-
-            grads = tape.gradient(loss, self.trainable_variables)
-
-            if gradients is None:
-                gradients = [grad*float(yy.shape[0]) for grad in grads]
-            else:
-                gradients = [gradients[i]+grad*float(yy.shape[0]) for i, grad in enumerate(grads)]
-            grads = None
-        gradients = [grad/float(y.shape[0]) for grad in gradients]
-
-        # Update weights
-        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-        # Update metrics (includes the metric that tracks the loss)
-        for metric in self.metrics:
-            if metric.name == "loss":
-                metric.update_state(loss)
-            else:
-                metric.update_state(y, y_pred)
-        # Return a dict mapping metric names to current value
-        return {m.name: m.result() for m in self.metrics}
+# class FullBatchModel(tf.keras.Model):
+#     def train_step(self, data):
+#         # Unpack the data. Its structure depends on your model and
+#         # on what you pass to `fit()`.
+#         if len(data) == 3:
+#             x, y, sample_weight = data
+#         else:
+#             sample_weight = None
+#             x, y = data
+#
+#         small_batch = 10
+#         gradients = None
+#         for i in range(0, y.shape[0], small_batch):
+#             xx = x[i:i+small_batch]
+#             yy = y[i:i+small_batch]
+#             if sample_weight is None:
+#                 ss = None
+#             else:
+#                 ss = sample_weight[i:i+small_batch]
+#             with tf.GradientTape() as tape:
+#                 y_pred = self(xx, training=True)  # Forward pass
+#                 # Compute the loss value
+#                 # (the loss function is configured in `compile()`)
+#                 loss = self.compute_loss(y=yy, y_pred=y_pred, sample_weight=ss)
+#
+#             grads = tape.gradient(loss, self.trainable_variables)
+#
+#             if gradients is None:
+#                 gradients = [grad*float(yy.shape[0]) for grad in grads]
+#             else:
+#                 gradients = [gradients[i]+grad*float(yy.shape[0]) for i, grad in enumerate(grads)]
+#             grads = None
+#         gradients = [grad/float(y.shape[0]) for grad in gradients]
+#
+#         # Update weights
+#         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+#         # Update metrics (includes the metric that tracks the loss)
+#         for metric in self.metrics:
+#             if metric.name == "loss":
+#                 metric.update_state(loss)
+#             else:
+#                 metric.update_state(y, y_pred)
+#         # Return a dict mapping metric names to current value
+#         return {m.name: m.result() for m in self.metrics}
