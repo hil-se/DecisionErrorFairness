cb1afedcff487d5a93ab344b253095b7db5e096a
azhe825 <zyu9@ncsu.edu>
2024-04-03 13:57:49 -0400

    random

--- face/src/rft.py
+++ face/src/rft.py
@@ -36,7 +36,7 @@ class RelativeFairnessTesting():
 
             m = Metrics(self.data[base][train], preds[train])
             result = {"Pair": base, "Metric": "Train"}
-            result["Accuracy"] = 1.0 - m.mae()
+            result["Accuracy"] = "%.2f" %(1.0 - m.mae())
             for A in self.protected:
                 result[A] = "(%.2f) %.2f" % (m.RBT(self.data[A][train]), m.RBD(self.data[A][train]))
             results.append(result)
@@ -45,28 +45,28 @@ class RelativeFairnessTesting():
                 # GT on training set
                 result = {"Pair": base+"/"+target, "Metric": "GT Train"}
                 m = Metrics(self.data[target][training], self.data[base][training])
-                result["Accuracy"] = 1.0 - m.mae()
+                result["Accuracy"] = "%.2f" %(1.0 - m.mae())
                 for A in self.protected:
                     result[A] = "(%.2f) %.2f" % (m.RBT(self.data[A][training]), m.RBD(self.data[A][training]))
                 results.append(result)
                 # GT on test set
                 result = {"Pair": base+"/"+target, "Metric": "GT Test"}
                 m = Metrics(self.data[target][test], self.data[base][test])
-                result["Accuracy"] = 1.0 - m.mae()
+                result["Accuracy"] = "%.2f" %(1.0 - m.mae())
                 for A in self.protected:
                     result[A] = "(%.2f) %.2f" % (m.RBT(self.data[A][test]), m.RBD(self.data[A][test]))
                 results.append(result)
                 # Prediction on test set
                 result = {"Pair": base + "/" + target, "Metric": "Unbiased Bridge"}
                 m = Metrics(self.data[target][test], preds[test])
-                result["Accuracy"] = 1.0 - m.mae()
+                result["Accuracy"] = "%.2f" %(1.0 - m.mae())
                 for A in self.protected:
                     result[A] = "(%.2f) %.2f" % (m.RBT(self.data[A][test]), m.RBD(self.data[A][test]))
                 results.append(result)
                 # predict test
                 result = {"Pair": base + "/" + target, "Metric": "Biased Bridge"}
                 m = BiasedBridge(preds[val] - self.data[base][val], preds[test] - self.data[target][test].to_numpy())
-                result["Accuracy"] = 1.0
+                result["Accuracy"] = "%.2f" %(1.0)
                 for A in self.protected:
                     result[A] = "(%.2f) %.2f" % (
                     m.RBT(self.data[A][val], self.data[A][test]), m.RBD(self.data[A][val], self.data[A][test]))
